d75114de534dedae4940a831dba279f6f182d40f
ipshita2306 <ichaturvedi2306@gmail.com>
2022-07-07 13:04:13 +0530

    Initial Commit

---
+++ SwiftDemoIpshita/SwiftDemoIpshitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoIpshitaApp.swift
+//  SwiftDemoIpshita
+//
+//  Created by Pradeep Chaturvedi on 07/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoIpshitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
